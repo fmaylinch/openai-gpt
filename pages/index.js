ba9d9b6d742632829119a31cbadf7afdea46999c
Ferran Maylinch <ferran@yandex-team.ru>
2023-07-07 12:09:17 +0300

    Add react-syntax-highlighter

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import { useState } from "react";
 import styles from "./index.module.css";
+import SyntaxHighlighter from 'react-syntax-highlighter';
+//import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
 export default function Home() {
   const [msgInput, setMsgInput] = useState("user: ");
@@ -13,6 +15,7 @@ export default function Home() {
 
     try {
       setResult(["Sending request..."])
+      const startDate = new Date();
       const response = await fetch("/api/generate", {
         method: "POST",
         headers: {
@@ -22,12 +25,14 @@ export default function Home() {
       });
 
       const data = await response.json();
+      const endDate = new Date();
       if (response.status !== 200) {
         throw data.error || new Error(`Request failed with status ${response.status}`);
       }
 
-      setResult(splitContent(data.message.content));
-      setMsgInput(msgInput + "\n\n" + data.message.role + ":\n" + data.message.content );
+      const durationMessage = `(Got response from ${model} in ${endDate - startDate} ms)`;
+      setResult([durationMessage, ...splitContent(data.message.content)]);
+      //setMsgInput(msgInput + "\n\n" + data.message.role + ":\n" + data.message.content );
     } catch(error) {
       console.error(error);
       setResult(["ERROR", JSON.stringify(error)]);
@@ -37,21 +42,28 @@ export default function Home() {
   function stringsToDomElems(strings) {
     let result = [];
     let code = null;
+    let lang = null;
     strings.forEach(str => {
-      if (str.startsWith('- ')) {
-        result.push(<li>{str.substring(2)}</li>);
-      } else if (str.startsWith('- ')) {
-        result.push(<li>{str.substring(2)}</li>);
-      } else if (str.startsWith('```')) {
+      if (str.startsWith('```')) {
         if (code == null) {
           code = ""; // start code block
+          lang = str.sub(3);
         } else {
-          result.push(<pre className={styles.code}>{code}</pre>)
+          //result.push(<pre className={styles.code}>{code}</pre>)
+          result.push(<SyntaxHighlighter language={lang}>{code}</SyntaxHighlighter>)
           code = null; // finish code block
         }
       } else {
         if (code == null) {
-          result.push(<p>{str}</p>);
+          if (str.startsWith('- ')) {
+            result.push(<li>{str.substring(2)}</li>);
+          } else if (str.startsWith('- ')) {
+            result.push(<li>{str.substring(2)}</li>);
+          } else if (str.startsWith('(') && str.endsWith(')')) { // for durationMessage
+            result.push(<p className={styles.comment}>{str}</p>);
+          } else {
+            result.push(<p>{str}</p>);
+          }
         } else {
           code += str + "\n"; // building code block
         }
@@ -61,7 +73,7 @@ export default function Home() {
   }
 
   return (
-    <div className={styles.full}>
+    <div>
       <Head>
         <title>OpenAI Quickstart</title>
         <link rel="icon" href="/dog.png" />
